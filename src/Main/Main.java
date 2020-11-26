2b87b97e8d2df34eadc41110a54eed17e9e4aaea
lixiaofei251 <2976030583@qq.com>
2020-11-26 19:02:23 +0800

    我的Git02项目-主干合并了分支

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("1.Git02");
         System.out.println("新版本");
         System.out.println("分支代码");
+        System.out.println("分支之后主分支的代码");
     }
 }
