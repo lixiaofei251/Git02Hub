ff4d57b205ad5c4c8fed022e46c4a508bbf35b8a
lixiaofei251 <2976030583@qq.com>
2020-11-26 19:06:43 +0800

    我的Git02项目-主干提交了代码

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -8,7 +8,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("1.Git02");
         System.out.println("新版本");
-        System.out.println("分支代码");
-        System.out.println("分支之后主分支的代码");
     }
 }
